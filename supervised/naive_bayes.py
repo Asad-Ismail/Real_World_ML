954c799df2fe81aa8099419af226bc674ccfca7e
Asad-Ismail <asadismaeel@gmail.com>
2023-03-01 21:19:08 +0000

    updated naive bayes

--- supervised/naive_bayes.py
+++ supervised/naive_bayes.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def get_data():
     from sklearn.datasets import make_blobs
@@ -7,17 +7,26 @@ def get_data():
     X, Y = make_blobs(n_samples=100, centers=2, random_state=42)
     return X,Y
 
-
-def hinge_loss(y_true, y_pred):
-    return np.maximum(0, 1 - y_true * y_pred)
-
-class SVM:
-    def __init__(self, learning_rate=0.001, lambda_param=1, num_iterations=100000):
-        self.learning_rate = learning_rate
-        self.lambda_param = lambda_param
-        self.num_iterations = num_iterations
-        self.w = None
-        self.b = None
+class NaiveBayes:
+    def __init__(self):
+        pass
+    
+    def prior_prob(y):
+        """
+        Calculate the prior probability of each class.
+        
+        Arguments:
+        y -- a numpy array of shape (n_samples,) containing the class labels
+        
+        Returns:
+        a numpy array of shape (n_classes,) containing the prior probability of each class
+        """
+        n_samples = y.shape[0]
+        n_classes = len(np.unique(y))
+        prior = np.zeros(n_classes)
+        for i in range(n_classes):
+            prior[i] = np.sum(y == i) / n_samples
+        return prior
     
     def fit(self, X, y,num_epochs=100):
         n_samples, n_features = X.shape
@@ -52,31 +61,5 @@ class SVM:
         y_pred = np.dot(X, self.w) + self.b
         return np.sign(y_pred)
 
-
-def plot_decision_boundary(model, X, y):
-    x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
-    x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
-    xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, 0.1), np.arange(x2_min, x2_max, 0.1))
-    Z = model.predict(np.c_[xx1.ravel(), xx2.ravel()])
-    Z = Z.reshape(xx1.shape)
-    plt.contourf(xx1, xx2, Z, alpha=0.4)
-    plt.scatter(X[:, 0], X[:, 1], c=y, alpha=0.8)
-    plt.show()
-    plt.savefig('results/svm.png')
-
-  
-if __name__=="__main__":
-
-    X,Y=get_data()
-    Y=np.where(Y==0,-1,1)
-    print(f"Y min and max are {Y.min()},{Y.max()}")
-    plt.scatter(X[:, 0], X[:, 1], c=Y)
-    plt.savefig('results/data.png')
-    svm=SVM()
-    svm.fit(X, Y)
-    plot_decision_boundary(svm, X, Y)
-    y_pred = svm.predict(X)
-    accuracy = np.sum(y_pred == Y) / len(Y)
-    print(f"Accuracy: {accuracy}")
 
 
