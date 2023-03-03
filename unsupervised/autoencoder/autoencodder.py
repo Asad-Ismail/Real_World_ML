0a9bd41006c8d4f8b3088948cb5f6bc51da64669
Asad-Ismail <asadismaeel@gmail.com>
2023-03-03 21:30:21 +0000

    fixed autoencoder

--- unsupervised/autoencoder/autoencodder.py
+++ unsupervised/autoencoder/autoencodder.py
@@ -3,9 +3,7 @@ import torch.nn as nn
 import torchvision
 
 # Define the transformation to apply to each image
-transform = transforms.Compose(
-    [transforms.ToTensor()]
-    ) 
+transform = transforms.Compose([transforms.ToTensor()]) 
 
 # Load the training and test datasets
 train_dataset = torchvision.datasets.MNIST(root='./data', train=True,
@@ -31,23 +29,26 @@ class Autoencoder(nn.Module):
 # Instantiate the autoencoder
 input_size = 784 # MNIST images are 28x28, so the input size is 784
 hidden_size = 64
-model = Autoencoder(input_size, hidden_size)
 
-# Define the loss function and optimizer
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+if __name__=="__main__":
+    model = Autoencoder(input_size, hidden_size)
+    print(f"Reduction in parameters {(64/784)*100}")
+    # Define the loss function and optimizer
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    # Train the autoencoder
+    num_epochs = 1
+    batch_size = 64
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    for epoch in range(num_epochs):
+        for data in train_loader:
+            img, _ = data
+            img = img.view(img.size(0), -1)
+            recon = model(img)
+            loss = criterion(recon, img)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
+    
 
-# Train the autoencoder
-num_epochs = 10
-batch_size = 64
-train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-for epoch in range(num_epochs):
-    for data in train_loader:
-        img, _ = data
-        img = img.view(img.size(0), -1)
-        recon = model(img)
-        loss = criterion(recon, img)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-    print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, loss.item()))
