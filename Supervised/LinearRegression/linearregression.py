5e62b059b321e52a054e4676c3b5546d9a53a6fa
Asad-Ismail <asadismaeel@gmail.com>
2023-03-13 21:40:49 +0000

    Updated linear regression

--- Supervised/LinearRegression/linearregression.py
+++ Supervised/LinearRegression/linearregression.py
@@ -2,7 +2,9 @@ import numpy as np
 
 class LinearRegression:
     
-    def __init__(self):
+    def __init__(self, alpha=1.0):
+        ## Alpha indicates regularizer 
+        self.alpha = alpha
         self.intercept_ = None
         self.coef_ = None
         
@@ -10,8 +12,10 @@ class LinearRegression:
         # Add a column of ones to X to represent the intercept term
         X = np.hstack((np.ones((X.shape[0], 1)), X))
         
-        # Calculate the OLS coefficients
-        self.coef_ = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
+        # Calculate the ridge regression coefficients
+        I = np.eye(X.shape[1])
+        I[0,0] = 0 # Don't regularize the intercept
+        self.coef_ = np.linalg.inv(X.T.dot(X) + self.alpha*I).dot(X.T).dot(y)
         self.intercept_ = self.coef_[0]
         self.coef_ = self.coef_[1:]
         
@@ -22,7 +26,6 @@ class LinearRegression:
         # Predict y using the learned coefficients
         y_pred = X.dot(np.hstack((self.intercept_, self.coef_)))
         return y_pred
-
 
 if __name__=="__main__":
     # Create a toy dataset with 2d feature vector
@@ -38,10 +41,10 @@ if __name__=="__main__":
     print("Coefficients: ", lr.coef_)
 
     # Use the model to make predictions on new data
-    X_new = np.array([[2, 3], [4, 5], [6, 7]])
-    y_pred = lr.predict(X_new)
+    #X_new = np.array([[2, 3], [4, 5], [6, 7]])
+    y_pred = lr.predict(X)
     print("Predictions: ", y_pred)
     # Calculate the residuals
-    residuals = np.sqrt((y - y_pred)**2)
+    residuals = np.mean(np.sqrt((y - y_pred)**2))
     # Print the residuals
-    print("Residuals: ", residuals)
+    print("Mean Residuals: ", residuals)
