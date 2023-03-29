8dc19fd60cf161b1b9996014eb7fde827897afdc
Asad-Ismail <asadismaeel@gmail.com>
2023-03-29 22:08:55 +0000

    added sarsa description

--- Reinforcement_Learning/sarsa.py
+++ Reinforcement_Learning/sarsa.py
@@ -10,4 +10,37 @@ Learning rate (α): Controls the step size of the Q-value updates. A smaller lea
 Discount factor (γ): Determines the importance of future rewards compared to immediate rewards. A value closer to 0 makes the agent focus on immediate rewards, while a value closer to 1 makes the agent consider long-term rewards.
 """
 
+import numpy as np
+
+class Gridworld:
+    def __init__(self, size=4, start=(0, 0), goal=(3, 3)):
+        self.size = size
+        self.start = start
+        self.goal = goal
+        self.state = start
+
+    def step(self, action):
+        x, y = self.state
+
+        if action == 0:  # up
+            y = max(y - 1, 0)
+        elif action == 1:  # right
+            x = min(x + 1, self.size - 1)
+        elif action == 2:  # down
+            y = min(y + 1, self.size - 1)
+        elif action == 3:  # left
+            x = max(x - 1, 0)
+
+        self.state = (x, y)
+
+        reward = -1
+        if self.state == self.goal:
+            reward = 0
+
+        return self.state, reward
+
+    def reset(self):
+        self.state = self.start
+        return self.state
+
 
